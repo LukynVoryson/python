e2814bc53b4c5adee27dfba5e21946e85e5aee8a
LukynVoryson <lukas.voracek@infis.cz>
2025-05-13 08:38:15 +0200

    Add files via upload

---
+++ procvicovani AAP pole.py
@@ -0,0 +1,33 @@
+ovoce=["Jablko","banán","pomeranč","kiwi","hruška"]
+
+print(ovoce[0])   # Jablko
+print(ovoce[2])   # pomeranč
+print(ovoce[-1])  # hruška
+
+
+jidlo= ["šunkofleky","řízek","rajská"]
+
+print(jidlo)
+
+
+znamky= [1,1,2,4,2,2,1,1]
+
+
+zvirata= ["Lev","Puma","Mědvěd","žralok tygří"]
+
+print(zvirata[1],zvirata[3])
+
+
+seznam=[]
+
+seznam.append(1)
+seznam.append(2)
+seznam.append(3)
+
+print(seznam)
+
+
+filmy=["Minecraft movie","Towers","finding water"]
+velikost=len(filmy)
+for i in range (velikost):
+    print(filmy[i])
